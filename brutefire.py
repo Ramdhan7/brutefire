af1f9292b187cbc0f12c2ab9bd3945cf84a2dfca
Ramdhan Ramadhian ID <75580821+Ramdhan7@users.noreply.github.com>
2022-09-21 03:41:28 +0700

    Update brutefire.py

--- brutefire.py
+++ brutefire.py
@@ -25,7 +25,7 @@ logo = """ \033[1;91m
               :hds+/-`          `-/+sdh:
                 `/ymM+          oMmy:\033[1;97m
                   [\033[41;1m BRUTEFORHACK \033[00;1m]
-                           \n          [1;96 Created By Ramdhan Ramadhian
+                           \n        \033[1;96m Created By Ramdhan Ramadhian
 """
 
 print (logo)
